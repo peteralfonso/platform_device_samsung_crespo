d8007979f4d72538c09a7a9695f641b5e74cfc04
Peter Alfonso <peteralfonsojr@gmail.com>
2012-01-05 12:18:49 -0500

    Allow dev key NFC access on user build variants

--- device_base.mk
+++ device_base.mk
@@ -99,13 +99,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-	NFCEE_ACCESS_PATH := device/samsung/crespo/nfcee_access.xml
-else
-	NFCEE_ACCESS_PATH := device/samsung/crespo/nfcee_access_debug.xml
-endif
 PRODUCT_COPY_FILES += \
-	$(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+	device/samsung/crespo/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Generated kcm keymaps
 PRODUCT_PACKAGES += \
